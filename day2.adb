05c6d29c3cfa38122f1d332f04e849f711f6048c
Mukhamed Karanashev <menchauser@gmail.com>
2020-12-03 17:26:41 +0300

    Use library functions

--- day2.adb
+++ day2.adb
@@ -1,33 +1,27 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Strings; use Ada.Strings;
-with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Strings.Fixed; 
+with Ada.Strings.Maps;
 
 procedure Day2 is
     InputFile : File_Type;
     Result    : Natural := 0;
 
     function Is_Valid (PasswordLine: String) return Boolean is 
+        package F renames Ada.Strings.Fixed;
+        package M renames Ada.Strings.Maps;
         -- PasswordLine format: %d-%d %c: %s
-        PolicyDelimIdx : Natural := Index (PasswordLine, "-");
-        PolicyEndIdx   : Natural := Index (PasswordLine, " ");
+        PolicyDelimIdx : Natural := F.Index (PasswordLine, "-");
+        PolicyEndIdx   : Natural := F.Index (PasswordLine, " ");
         MinChars       : Natural := Natural'Value (PasswordLine (PasswordLine'First..(PolicyDelimIdx-1)));
         MaxChars       : Natural := Natural'Value (PasswordLine ((PolicyDelimIdx+1)..PolicyEndIdx));
         Char           : Character := PasswordLine(PolicyEndIdx+1);
-        PasswordIdx    : Natural := Index (PasswordLine, " ", Backward) + 1;
+        PasswordIdx    : Natural := F.Index (PasswordLine, " ", Backward) + 1;
         Password       : String := PasswordLine (PasswordIdx..PasswordLine'Last);
-        CharCount      : Natural := 0;
+        CharCount      : Natural := F.Count (Password, M.To_Set (Char));
     begin
-        for I in Password'Range loop
-            if Password(I) = Char then
-                CharCount := CharCount + 1;
-            end if;
-        end loop;
-        if CharCount in MinChars .. MaxChars then
-            return True;
-        else 
-            return False;
-        end if;
+        return  CharCount in MinChars .. MaxChars;
     end Is_Valid;
 begin
     Open (InputFile, In_File, "input2.txt");
