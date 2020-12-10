eec841e2a5fce4dd53d67faf2c95fa8dfc2034f6
Mukhamed Karanashev <menchauser@gmail.com>
2020-12-10 20:29:01 +0300

    Dirty solution for Day 10 Part 2

--- day10.adb
+++ day10.adb
@@ -1,10 +1,12 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Long_Integer_Text_IO; use Ada.Long_Integer_Text_IO;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Maps; use Ada.Strings.Maps;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 with Ada.Containers.Vectors;
+with Ada.Command_Line; 
 
 
 procedure Day10 is
@@ -13,6 +15,20 @@ procedure Day10 is
          Index_Type   => Natural);
     package Int_Vector_Sorter is new Int_Vector_T.Generic_Sorting;
     
+    procedure Put (V : Int_Vector_T.Vector) is
+        First : Boolean := True;
+    begin
+        for X of V loop
+            if First then
+                First := False;
+            else
+                Put (' ');
+            end if;
+            Put (X, 0);
+        end loop;
+    end;
+
+
     -- Main variables
     InputFile : File_Type;
     Line      : Unbounded_String;
@@ -22,7 +38,7 @@ procedure Day10 is
     Diff_Counts : array (1 .. 3) of Natural := (others => 0);
 begin
     -- Read File
-    Open (InputFile, In_File, "input10.txt");
+    Open (InputFile, In_File, Ada.Command_Line.Argument (1));
     loop 
         Get (InputFile, Next);
         Input.Append (Next);
@@ -33,9 +49,7 @@ begin
     Int_Vector_Sorter.Sort (Input);
 
     Put_Line ("Sorted vector: ");
-    for I in Input.First_Index .. Input.Last_Index loop
-        Put (Input (I), 0); Put (' '); 
-    end loop;
+    Put (Input);
     New_Line;
 
     declare 
@@ -59,6 +73,49 @@ begin
         Put (Diff_Counts(I));
         New_Line;
     end loop;
-    Put ("Result: ");
+    Put ("Part 1 result: ");
     Put (Diff_Counts(1) * Diff_Counts(3));
+    New_Line;
+
+    -- For part 2 we can start to go from end and remove 1-2 diffs
+    -- Test_Permutations (Input);
+    -- let's find out longest groups of succeeding numbers
+    declare 
+        Diff : Integer := 0;
+        GroupLength : Natural := 1;
+        Result : Long_Integer := 1;
+        Multipliers : array (1 .. 5) of Long_Integer := (1, 1, 2, 4, 7);
+    begin
+        Input.Prepend (0);
+        Input.Append (Input.Last_Element + 3);
+        Put (Input); New_Line;
+        for I in Input.First_Index .. Input.Last_Index - 1 loop
+            Diff := Input(I + 1) - Input(I);
+            if Input(I + 1) - Input(I) = 1 then
+                GroupLength := GroupLength + 1;
+            else
+                Put ("Group finished, length: ");
+                Put (GroupLength);
+                New_Line;
+                if GroupLength > 1 then
+                    Result := Result * Multipliers(GroupLength);
+                    GroupLength := 1;
+                end if;
+            end if;
+        end loop;
+        if GroupLength > 0 then
+            Put ("Group finished, length: ");
+            Put (GroupLength);
+            New_Line;
+            if GroupLength > 1 then
+                Result := Result * Multipliers(GroupLength);
+            end if;
+        end if;
+
+        Put ("Part 2 Result: ");
+        Put (Result);
+        New_Line;
+    end;
+
+
 end Day10;
