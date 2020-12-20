0bea5501b7a76de542523f4c59b83302a19e3106
Mukhamed Karanashev <menchauser@gmail.com>
2020-12-20 12:08:44 +0300

    New step: tokenize

--- day18.adb
+++ day18.adb
@@ -12,7 +12,8 @@ with Ada.Command_Line;
 
 procedure Day18 is
     Digit_Chars : constant Character_Set := To_Set ("1234567890");
-    Op_Chars    : constant Character_Set := To_Set ("+*(");
+    Op_Chars    : constant Character_Set := To_Set ("+*");
+    Brace_Chars : constant Character_Set := To_Set ("()");
 
     type Node_Class is (Number, Op);
 
@@ -26,23 +27,33 @@ procedure Day18 is
     end record;
 
 
-    type Op_Token is record
-        Op    : Character;
-        Depth : Natural;
+    type Token_Class is (Number, Op, LBrace, RBrace);
+
+    type Token (Class : Token_Class) is record
+        case Class is
+            when Number =>
+                Value : Long_Integer;
+            when Op =>
+                Op : Character;
+            when LBrace =>
+                LBrace : Character;
+            when RBrace =>
+                RBrace : Character;
+        end case;
     end record;
     
 
     package Long_Int_Vec is new Ada.Containers.Vectors
         (Index_Type   => Natural,
          Element_Type => Long_Integer);
-
-    package Op_Vec is new Ada.Containers.Vectors
-        (Index_Type   => Natural,
-         Element_Type => Op_Token);
     
     package Char_Vec is new Ada.Containers.Vectors
         (Index_Type   => Natural,
          Element_Type => Character);
+
+    package Token_Vec is new Ada.Containers.Indefinite_Vectors
+        (Index_Type   => Natural,
+         Element_Type => Token);
 
     package Node_Vec is new Ada.Containers.Indefinite_Vectors
         (Index_Type   => Natural,
@@ -52,7 +63,6 @@ procedure Day18 is
     procedure Put (V : Node_Vec.Vector) is
         First : Boolean := True;
     begin
-        Put ("RPN: ");
         for X of V loop
             if First then
                 First := False;
@@ -69,79 +79,122 @@ procedure Day18 is
     end;
 
 
-    function Parse_To_RPN (Expr : Unbounded_String) return Node_Vec.Vector is
-        -- Input      : Unbounded_String := Expr & ' ';
-        Result     : Node_Vec.Vector := Node_Vec.Empty_Vector;
-        Op_Stack   : Char_Vec.Vector := Char_Vec.Empty_Vector;
-        I          : Natural := 1;
-        C          : Character;
-        Token      : Unbounded_String;
+    procedure Put (V : Token_Vec.Vector) is
+        First : Boolean := True;
+    begin
+        for X of V loop
+            if First then
+                First := False;
+            else
+                Put (' ');
+            end if;
+            case X.Class is
+                when Number =>
+                    Put ("Num["); Put (X.Value, 0); Put ("]");
+                when Op =>
+                    Put ("Op["); Put (X.Op); Put ("]");
+                when LBrace =>
+                    Put ("(");
+                when RBrace =>
+                    Put (")");
+            end case;
+        end loop;
+    end;
+
+
+    function Precedence1 (Op : Character) return Natural is
+    begin 
+        case Op is
+            when '+' => return 1;
+            when '*' => return 1;
+            when '(' => return 0;
+            when others =>
+                raise Program_Error;
+        end case;
+    end Precedence1;
+
+
+    function Tokenize (Expr : Unbounded_String) return Token_Vec.Vector is
+        Result : Token_Vec.Vector := Token_Vec.Empty_Vector;
+        Token  : Unbounded_String;
+        I      : Natural := 1;
+        C      : Character; 
     begin
         loop
             C := Element (Expr, I);
             if Is_In (C, Digit_Chars) then
                 Token := Token & C;
             elsif Is_In (C, Op_Chars) then
-                Op_Stack.Append (C);
+                Result.Append ((Class => Op, Op => C));
+            elsif C = '(' then
+                Result.Append ((Class => LBrace, LBrace => C));
             elsif C = ')' then
                 if Token /= "" then
-                    -- last token was number, add it to result
-                    -- also add token from op stack, if exists
                     Result.Append ((
                         Class => Number,
                         Value => Long_Integer'Value (To_String (Token))
                     ));
                     Token := Null_Unbounded_String;
                 end if;
-                loop
-                    C := Op_Stack (Op_Stack.Last_Index);
-                    Op_Stack.Delete_Last;
-                    exit when C = '(';
-                    Result.Append ((Class => Op, Op => C));
-                end loop;
-                if not Op_Stack.Is_Empty 
-                    and then Op_Stack.Last_Element /= '(' then 
-                    C := Op_Stack.Last_Element;
-                    Op_Stack.Delete_Last;
-                    Result.Append ((Class => Op, Op => C));
-                end if;
+                Result.Append ((Class => RBrace, RBrace => C));
             elsif C = ' ' then
                 if Token /= "" then
-                    -- last token was number, add it to result
-                    -- also add token from op stack, if exists
                     Result.Append ((
                         Class => Number,
                         Value => Long_Integer'Value (To_String (Token))
                     ));
                     Token := Null_Unbounded_String;
-                    if not Op_Stack.Is_Empty 
-                        and then Op_Stack.Last_Element /= '(' then 
-                        C := Op_Stack.Last_Element;
-                        Op_Stack.Delete_Last;
-                        Result.Append ((Class => Op, Op => C));
-                    end if;
                 end if;
             end if;
             I := I + 1;
             if I > Length (Expr) then
                 if Token /= "" then
-                    -- check last number at the end of string
                     Result.Append ((
-                        Class => Number, 
+                        Class => Number,
                         Value => Long_Integer'Value (To_String (Token))
                     ));
-                    Token := Null_Unbounded_String;
                 end if;
-                while not Op_Stack.Is_Empty loop
-                    C := Op_Stack.Last_Element;
-                    Op_Stack.Delete_Last;
-                    Result.Append ((Class => Op, Op => C));
-                end loop;
                 exit;
             end if;
         end loop;
         return Result;
-    end Parse_To_RPN;
+    end Tokenize;
+
+
+    function To_RPN (Tokens : Token_Vec.Vector) return Node_Vec.Vector is
+        Result     : Node_Vec.Vector := Node_Vec.Empty_Vector;
+        Op_Stack   : Char_Vec.Vector := Char_Vec.Empty_Vector;
+        C          : Character;
+    begin
+        for T of Tokens loop
+            case T.Class is
+                when Number =>
+                    Result.Append ((Class => Number, Value => T.Value));
+                    if not Op_Stack.Is_Empty 
+                        and then Op_Stack.Last_Element /= '(' then
+                        Result.Append ((Class => Op, Op => Op_Stack.Last_Element));
+                        Op_Stack.Delete_Last;
+                    end if;
+                when Op =>
+                    Op_Stack.Append (T.Op);
+                when LBrace =>
+                    Op_Stack.Append ('(');
+                when RBrace =>
+                    loop
+                        C := Op_Stack.Last_Element;
+                        Op_Stack.Delete_Last;
+                        exit when C = '(';
+                        Result.Append ((Class => Op, Op => C));
+                    end loop;
+                    if not Op_Stack.Is_Empty 
+                        and then Op_Stack.Last_Element /= '(' then
+                        Result.Append ((Class => Op, Op => Op_Stack.Last_Element));
+                        Op_Stack.Delete_Last;
+                    end if;
+            end case;
+        end loop;
+        return Result;
+    end To_RPN;
 
 
     function Eval_RPN (Expr : Node_Vec.Vector) return Long_Integer is
@@ -173,6 +226,7 @@ procedure Day18 is
     -- Main variables
     File   : File_Type;
     Line   : Unbounded_String;
+    Parsed : Token_Vec.Vector;
     RPN    : Node_Vec.Vector;
     Value  : Long_Integer;
     Result : Long_Integer := 0;
@@ -183,8 +237,10 @@ begin
         Get_Line (File, Line);
         if Element (Line, 1) /= '#' then
             Put_Line (Line);
-            RPN := Parse_To_RPN (Line);
-            Put (" => "); Put (RPN); New_Line;
+            Parsed := Tokenize (Line);
+            -- Put ("Tokens: "); Put (Parsed); New_Line;
+            RPN := To_RPN (Parsed);
+            Put ("RPN: "); Put (RPN); New_Line;
             Value := Eval_RPN (RPN);
             Put (" = "); Put (Value, 0);
             Result := Result + Value;
